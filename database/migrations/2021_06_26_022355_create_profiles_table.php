9aa00243f33ef3dfec1db0fa8c1cce898a4515e7
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-06-26 10:11:33 +0700

    Update 2021_06_26_022355_create_profiles_table.php

--- database/migrations/2021_06_26_022355_create_profiles_table.php
+++ database/migrations/2021_06_26_022355_create_profiles_table.php
@@ -15,6 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('uid');
             $table->string('address');
             $table->string('phone');
             $table->string('img_url');
